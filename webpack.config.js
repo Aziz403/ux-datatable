6bf7c21dca2f22a3593c46bb3c9aa184e23a273a
aziz benmallouk <azizbenmallouk4@gmail.com>
2023-01-11 13:16:46 +0000

    config webpack & tailwindcss

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+module.exports = {
+    mode: 'production',
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
+    },
+    devServer: {
+        static: 'dist',
+    },
+};
