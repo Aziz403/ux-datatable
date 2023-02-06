3a3e96c7aaed08b6ac33a0e1f05bf853111505b8
aziz benmallouk <azizbenmallouk4@gmail.com>
2023-02-06 13:30:59 +0000

    update DataService::getPropValue

--- src/Service/DataService.php
+++ src/Service/DataService.php
@@ -6,23 +6,29 @@ use Aziz403\UX\Datatable\Exception\GetterNotFoundException;
 
 class DataService
 {
-    public static function getPropValue($object,$propName)
+    public static function getPropValue(object $object,string $property,array $argements = [])
     {
-        $getType = [$object,"get".ucfirst($propName)];
-        $isType = [$object,"is".ucfirst($propName)];
-        $hasType = [$object,"has".ucfirst($propName)];
-        if(is_callable($getType)){
-            $value = call_user_func_array($getType,[]);
+        $class = \get_class($object);
+
+        $methods = get_class_methods($object);
+        $method = array_filter($methods,function ($method) use($property){
+            if ('g' === $method[0] && 0 === strpos($method, 'get')) {
+                $name = substr($method, 3);
+            } elseif ('i' === $method[0] && 0 === strpos($method, 'is')) {
+                $name = substr($method, 2);
+            }
+
+            if(isset($name) && lcfirst($name)===$property){
+                return $method;
+            }
+        });
+
+        if (count($method)>0) {
+            $method = array_values($method)[0];
+        } else {
+            throw new \Exception(sprintf('Neither the property "%1$s" nor one of the methods "%1$s()", "get%1$s()"/"is%1$s()"/"has%1$s()" or "__call()" exist and have public access in class "%2$s".', $property, $class));
         }
-        elseif(is_callable($isType)){
-            $value = call_user_func_array($isType,[]);
-        }
-        elseif(is_callable($hasType)){
-            $value = call_user_func_array($hasType,[]);
-        }
-        else{
-            throw new GetterNotFoundException("Not Found Getter For $propName, With Name $getType[1],$isType[1] or $hasType[1]");
-        }
-        return $value;
+
+        return $object->$method(...$argements);
     }
 }
